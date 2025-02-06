d3ab984355a82be0d102a0ea5ddc8c7d51621da8
Supun Akalanka <153161364+supunakalanka76@users.noreply.github.com>
2025-02-06 23:28:21 +0530

    update

--- portfolio/src/components/home/Home.js
+++ portfolio/src/components/home/Home.js
@@ -6,7 +6,7 @@ import RightHome from './RightHome'
 function Home() {
 
   return (
-    <section id='home' className='lgl:w-full pl-10 pr-10 pt-16 pb-10 flex flex-col gap-10 xl:gap-0 lgl:flex-row border-b-[2px] font-poppins border-b-designColor'>
+    <section id='home' className='lgl:w-full pl-10 pr-10 lgl:pt-16 sm:pt-8 pb-10 flex flex-col gap-10 xl:gap-0 lgl:flex-row border-b-[2px] font-poppins border-b-designColor'>
 
       <LeftHome />
 
